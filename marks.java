730c68a1ad6635415c8bc041bc0f3ac20a17b2b7
Isha Savani <123747477+Ishasavani1402@users.noreply.github.com>
2024-02-18 19:31:11 +0530

    Add files via upload

---
+++ marks.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class marks {
+    public static void main(String[] args) {
+        Scanner marks = new Scanner(System.in);
+        System.out.println("enter marks of C & DSA:");
+        int m1 = marks.nextInt();
+        System.out.println("enter marks of statastical mathemetics:");
+        int m2 = marks.nextInt();
+        
+        System.out.println("enter marks of operating system:");
+        int m3 = marks.nextInt();
+        
+        System.out.println("enter marks of information technology");
+        int m4 = marks.nextInt();
+        
+        System.out.println("enter marks of communication skill:");
+        int m5 = marks.nextInt();
+        
+
+        int total = m1 + m2 + m3 + m4 + m5;
+        System.out.println("total obtain marks of all  subjects are : " + total);
+
+        float cgpa = total / 5.0f;
+        System.out.println("total CGPA is :" + cgpa);
+        marks.close();
+
+    }
+}
