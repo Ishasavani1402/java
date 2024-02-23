306a751a8451e299735e81d36c288a0ad4d96736
Isha Savani <123747477+Ishasavani1402@users.noreply.github.com>
2024-02-23 18:56:33 +0530

    Update marks.java

--- marks.java
+++ marks.java
@@ -7,16 +7,15 @@ public class marks {
         int m1 = marks.nextInt();
         System.out.println("enter marks of statastical mathemetics:");
         int m2 = marks.nextInt();
-        
+
         System.out.println("enter marks of operating system:");
         int m3 = marks.nextInt();
-        
+
         System.out.println("enter marks of information technology");
         int m4 = marks.nextInt();
-        
+
         System.out.println("enter marks of communication skill:");
         int m5 = marks.nextInt();
-        
 
         int total = m1 + m2 + m3 + m4 + m5;
         System.out.println("total obtain marks of all  subjects are : " + total);
@@ -24,6 +23,16 @@ public class marks {
         float cgpa = total / 5.0f;
         System.out.println("total CGPA is :" + cgpa);
         marks.close();
+
+        if (cgpa >= 80) {
+            System.out.println("first class with distinction");
+        } else if (cgpa >= 70) {
+            System.out.println("first class");
+        } else if (cgpa >= 60) {
+            System.out.println("second class");
+        } else if (cgpa >= 50) {
+            System.out.println("pass");
+        }
 
     }
 }
