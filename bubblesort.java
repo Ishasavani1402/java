8e3bc8f6882ec1d8f1ee97b90f195655fe6dffaa
Isha Savani <123747477+Ishasavani1402@users.noreply.github.com>
2024-03-08 20:46:46 +0530

    Update bubblesort.java

--- bubblesort.java
+++ bubblesort.java
@@ -1,22 +1,22 @@
 import java.util.Scanner;
 
 public class bubblesort {
-    public static void main(String[] args) {
+
+    public static void inputarray(int sort[], int length) {
         Scanner sc = new Scanner(System.in);
-
-        System.out.println("Enter length of array:");
-        int length = sc.nextInt();
-        int sort[] = new int[length];
-        System.out.println("enter" + " " + length + " " + "element:");
-
         for (int i = 0; i < sort.length; i++) {
             sort[i] = sc.nextInt();
-            // System.out.println(sort[i]);
         }
-        System.out.println("display array element:");
+        sc.close();
+    }
+
+    public static void displayarray(int sort[], int length) {
         for (int i = 0; i < sort.length; i++) {
             System.out.println(sort[i]);
         }
+    }
+
+    public static void bubblesorting(int sort[], int length) {
         for (int i = 0; i < sort.length - 1; i++)// number of passes
         {
             boolean issorted = true; // flag to check if the list is sorted or not
@@ -33,10 +33,21 @@ public class bubblesort {
             if (issorted == true)
                 break;
         }
-        System.out.println("\nSorted array in ascending order is : ");
-        for (int i = 0; i < sort.length; i++) {
-            System.out.print(sort[i] + " ");
-        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter length of array:");
+        int length = sc.nextInt();
+        int sort[] = new int[length];
+        System.out.println("enter" + " " + length + " " + "element:");
+        inputarray(sort, length);
+        System.out.println("before sorting:");
+        displayarray(sort, length);
+        bubblesorting(sort, length);
+        System.out.println("after sorting;");
+        displayarray(sort, length);
 
         sc.close();
 
