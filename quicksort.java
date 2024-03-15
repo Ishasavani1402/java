51a939eed915c278a0ca5384df962a54d495a372
Isha Savani <123747477+Ishasavani1402@users.noreply.github.com>
2024-03-15 20:19:00 +0530

    Update quicksort.java

--- quicksort.java
+++ quicksort.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class quicksort {
-    public static void inputarray(int qsort[], int length) {
+    static void inputarray(int qsort[], int length) {
         Scanner sc = new Scanner(System.in);
         for (int i = 0; i < qsort.length; i++) {
             qsort[i] = sc.nextInt();
@@ -9,67 +9,62 @@ public class quicksort {
         sc.close();
     }
 
-    public static void displayarray(int qsort[], int length) {
+    static void displayarray(int qsort[], int length) {
         for (int i = 0; i < qsort.length; i++) {
             System.out.println(qsort[i]);
         }
     }
-    
-    public static int partition(int qsort[],int low,int high)
-    {
+
+    static int partition(int qsort[], int low, int high) {
         int pivot = qsort[low]; // first element as pivot
-        int i=low+1;
-        int j=high;
+        int i = low + 1;
+        int j = high;
         do {
-            while (qsort[i]<=pivot) {
+            while (i <=high &&qsort[i] <= pivot) {
                 i++;
-                
+
             }
-            while(qsort[j]>pivot)
-            {
+            while (j>=low &&qsort[j] > pivot) {
                 j--;
             }
-            if(i<j)
-            {
-                //swap between i and j.
-                int temp =qsort[i];
+            if (i < j) {
+                // swap between i and j.
+                int temp = qsort[i];
                 qsort[i] = qsort[j];
                 qsort[j] = temp;
-            } 
-        } while (i<j);
-        //swap between pivot and j.
+            }
+        } while (i < j);
+        // swap between pivot and j.
         int temp = qsort[low];
-        qsort[low]=qsort[j];
-        qsort[j]=temp;
+        qsort[low] = qsort[j];
+        qsort[j] = temp;
         return j;
-        
+
     }
 
-    public static void performquicksort(int qsort[],int low,int high)
-    {
-        int partitionindex ;//  index of the pivot after partition
-        if(low<high)
-        {
-
-            partitionindex = partition(qsort, low, high);// get the partitioning index
-            performquicksort(qsort, low,partitionindex-1);// sort left subarray
-            performquicksort(qsort, partitionindex+1,high);//sort right subarray
+    static void performquicksort(int qsort[], int low, int high) {
+        // int partitionindex ;// index of the pivot after partition
+        if (low < high) {
+            int partitionindex = partition(qsort, low, high);// get the partitioning index
+            performquicksort(qsort, low, partitionindex - 1);// sort left subarray
+            performquicksort(qsort, partitionindex + 1, high);// sort right subarray
         }
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter length of array:");
         int length = sc.nextInt();
         int qsort[] = new int[length];
-        System.out.printf("enter %d element:",length);
+        System.out.printf("enter %d element:", length);
+    
         inputarray(qsort, length);
-        int low = 0,high=length-1;
+        int low = 0, high = length - 1;
         System.out.println("before sorting:");
         displayarray(qsort, length);
         performquicksort(qsort, low, high);
         System.out.println("after sorting:");
         displayarray(qsort, length);
-
         sc.close();
     }
 }
